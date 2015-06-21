e1ec8ae229721ed5d0132693ac65bb36e4fc9c31
alelievr <alelievr@student.42.fr>
2015-06-21 04:37:33 +0200

    all

--- src/PlayerShip.class.cpp
+++ src/PlayerShip.class.cpp
@@ -4,7 +4,14 @@ PlayerShip::PlayerShip(std::string name, int x, int y, int  hp , int maxhp) : Sh
 {
 	this->setMask(MASK_SHIP_LOW);
 	this->direction = 1;
+	this->_life = 5;
+	this->_isDead = false;
 	std::cout << "playership constructed : " << std::endl<< this->data << std::endl;
+}
+
+int			PlayerShip::getLife(void)
+{
+	return (this->_life);
 }
 
 void		PlayerShip::fire(void)
@@ -14,7 +21,14 @@ void		PlayerShip::fire(void)
 
 void		PlayerShip::die(void)
 {
-	
+	this->_life--;
+	if (this->_life < 0)
+		this->_isDead = true;
+}
+
+bool		PlayerShip::isAlive(void)
+{
+	return (!this->_isDead);
 }
 
 void		PlayerShip::move(int x, int y)
